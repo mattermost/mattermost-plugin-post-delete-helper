af340fef19021925063697a55b121bd5b78d59d0
Doug Lauder <wiggin77@warpmail.net>
2024-09-10 14:21:07 -0400

    Add menu allowing user to delete a root post without deleting the thread. (#1)
    * add menu; delete root post
    
    * only display menu when permissions are granted;  prevent root deleted posts from being modified
    
    * check if root post already deleted before displaying menu
    
    * configurable deleted message
    
    * go mod tidy

---
+++ server/hooks.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"time"
+
+	"github.com/mattermost/mattermost/server/public/model"
+	"github.com/mattermost/mattermost/server/public/plugin"
+)
+
+// MessageWillBeUpdated is invoked when a message is being updated
+func (p *Plugin) MessageWillBeUpdated(c *plugin.Context, newPost *model.Post, oldPost *model.Post) (*model.Post, string) {
+	// Do not allow deleted root posts to be modified.
+	if shouldPreventEdit(oldPost) {
+		return nil, "Deleted root post cannot be edited"
+	}
+	return newPost, ""
+}
+
+// ReactionHasBeenAdded is involked when a reaction has been added to a post.
+// There currently is no way to block adding a reaction (no `ReactionWillBeAdded`)
+// so we'll just delete the new reaction immediately to simulate a deleted post that
+// disallows reactions.
+func (p *Plugin) ReactionHasBeenAdded(c *plugin.Context, reaction *model.Reaction) {
+	if reaction == nil {
+		return
+	}
+
+	post, appErr := p.API.GetPost(reaction.PostId)
+	if appErr != nil {
+		// ignore the error and leave the reaction alone.
+		p.API.LogDebug("cannot fetch post to check for reaction blocking", "err", appErr.Error())
+		return
+	}
+
+	if !shouldPreventEdit(post) {
+		return
+	}
+
+	go func() {
+		// we need to give time for the add reaction websocket event to get sent, so the remove reaction
+		// websocket event will be sent behind it. Otherwise the websocket events may get sent in the wrong order
+		// and the client will display the deleted reaction until the page it refreshed.
+		time.Sleep(time.Millisecond * 250)
+
+		if appErr = p.API.RemoveReaction(reaction); appErr != nil {
+			p.API.LogError("cannot remove reaction", "err", appErr.Error())
+		}
+	}()
+}
+
+// shouldPreventEdit returns true if a post has been deleted as root, meaning it contains the property "del=true".
+func shouldPreventEdit(post *model.Post) bool {
+	// check if the post has the DeletedRootPostPropKey property set
+	val := post.GetProp(DeletedRootPostPropKey)
+	deleted, ok := val.(bool)
+	return ok && deleted
+}
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,15 +1,72 @@
-import {Store, Action} from 'redux';
+import {Action, Store} from 'redux';
 
-import {GlobalState} from '@mattermost/types/lib/store';
+import {Permissions} from 'mattermost-redux/constants';
+import {getCurrentUser} from 'mattermost-redux/selectors/entities/common';
+import {getConfig} from 'mattermost-redux/selectors/entities/general';
+import {getPost} from 'mattermost-redux/selectors/entities/posts';
+import {haveIChannelPermission} from 'mattermost-redux/selectors/entities/roles';
+import {getCurrentTeam} from 'mattermost-redux/selectors/entities/teams';
+import {GlobalState} from 'mattermost-redux/types/store';
 
 import manifest from '@/manifest';
 
 import {PluginRegistry} from '@/types/mattermost-webapp';
 
+import {triggerDeleteRootPostCommand} from './actions';
+
 export default class Plugin {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
     public async initialize(registry: PluginRegistry, store: Store<GlobalState, Action<Record<string, unknown>>>) {
         // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
+        registry.registerPostDropdownMenuAction(
+            'Remove root post',
+            async (postID) => {
+                store.dispatch(triggerDeleteRootPostCommand(postID) as any);
+            },
+            (postID) => {
+                const state = store.getState();
+                const post = getPost(state, postID);
+                if (!post) {
+                    return false;
+                }
+
+                // console.debug('reply_count=', post.reply_count, '  rootdel=', post.props?.rootdel, '  post_id=', post.id); //eslint-disable-line no-console
+
+                // check if post has replies
+                if (post.reply_count === 0) {
+                    return false;
+                }
+
+                // check if post is already marked as root post deleted
+                if (post.props && post.props.rootdel) {
+                    return false;
+                }
+
+                // Check if the user has permissions to edit his own post or edit other's posts if not the author
+                const user = getCurrentUser(state);
+                const team = getCurrentTeam(state);
+                let permission = Permissions.EDIT_POST;
+                if (post.user_id !== user.id) {
+                    permission = Permissions.EDIT_OTHERS_POSTS;
+                }
+                if (!haveIChannelPermission(state, {
+                    team: team.id,
+                    channel: post.channel_id,
+                    permission,
+                })) {
+                    return false;
+                }
+
+                // Check if post is editable
+                const config = getConfig(state);
+                const edit_time_limit : number = config.PostEditTimeLimit ? Number(config.PostEditTimeLimit) : -1;
+                if (edit_time_limit !== -1 && post.create_at + (edit_time_limit * 1000) < Date.now()) {
+                    return false;
+                }
+
+                return true;
+            },
+        );
     }
 }
 
